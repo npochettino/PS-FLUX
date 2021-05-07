7e6a9a413bca661046a22f442a6914fe4169b65a
Nicolás Pochettino <nicolas.pochettino@itsystemsinnova.com>
2021-05-07 19:26:16 -0300

    Course Form

--- src/components/App.js
+++ src/components/App.js
@@ -6,16 +6,20 @@ import CoursePage from "./CoursePage";
 import { Route, Switch, Redirect } from "react-router-dom";
 import NotFoundPage from "./NotFoundPAge";
 import ManageCoursePage from "./ManageCoursePage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
     <div className="container-fluid">
+      <ToastContainer autoClose={3000} hideProgressBar />
       <Header />
       <Switch>
         <Route path="/" exact component={HomePage} />
         <Route path="/courses" component={CoursePage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/course/:slug" component={ManageCoursePage} />
+        <Route path="/course" component={ManageCoursePage} />
         <Redirect from="/about-page" to="/about" />
         <Route component={NotFoundPage} />
       </Switch>
