3bb7fdbd96b234b3dc87bf2cca32d0d5d9eb62d8
Nicolás Pochettino <nicolas.pochettino@itsystemsinnova.com>
2021-05-10 10:56:53 -0300

    Flux

--- src/components/CourseList.js
+++ src/components/CourseList.js
@@ -1,11 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+
 function CourseList(props) {
   return (
     <table className="table">
       <thead>
         <tr>
+          <th>&nbsp;</th>
           <th>Title</th>
           <th>Author ID</th>
           <th>Category</th>
@@ -15,6 +17,14 @@ function CourseList(props) {
         {props.courses.map((course) => {
           return (
             <tr key={course.id}>
+              <td>
+                <button
+                  className="btn btn-outline-danger"
+                  onClick={() => props.deleteCourse(course.id)}
+                >
+                  Delete
+                </button>
+              </td>
               <td>
                 <Link to={"/course/" + course.slug}>{course.title}</Link>
               </td>
@@ -29,6 +39,7 @@ function CourseList(props) {
 }
 
 CourseList.propTypes = {
+  deleteCourse: PropTypes.func.isRequired,
   courses: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number.isRequired,
